8fbdb7e1f59e376231e5b32d05573401130ad28f
gustavo_richardt <gustavo_richardt@estudante.sesisenai.org.br>
2026-05-15 19:24:49 -0300

    entity e repository

---
+++ src/repository/ReservaRepository.java
@@ -0,0 +1,21 @@
+package repository;
+
+import entity.Reserva;
+import java.util.HashMap;
+
+public class ReservaRepository {
+
+    private HashMap<Integer, Reserva> reservas = new HashMap<>();
+
+    public void salvar(Reserva reserva) {
+        reservas.put(reserva.getId(), reserva);
+    }
+
+    public Reserva buscarPorId(int id) {
+        return reservas.get(id);
+    }
+
+    public HashMap<Integer, Reserva> listar() {
+        return reservas;
+    }
+}
---
+++ src/repository/SalaRepository.java
@@ -0,0 +1,21 @@
+package repository;
+
+import entity.Sala;
+import java.util.HashMap;
+
+public class SalaRepository {
+
+    HashMap<Integer, Sala> salas = new HashMap<>();
+
+    public void salvar(Sala sala) {
+        salas.put(sala.getId(), sala);
+    }
+
+    public Sala buscarPorId(int id) {
+        return salas.get(id);
+    }
+
+    public HashMap<Integer, Sala> listar() {
+        return salas;
+    }
+}
